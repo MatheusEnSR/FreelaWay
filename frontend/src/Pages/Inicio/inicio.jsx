2ff48256492ebe88939bd6c99857e12f8877531f
MatheusEnSR <matheusenriquesrodrigues@gmail.com>
2025-09-16 01:13:42 -0300

    Alteração na pagina Welcome

--- frontend/src/Pages/Inicio/inicio.jsx
+++ frontend/src/Pages/Inicio/inicio.jsx
@@ -1,47 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import './inicio.css';
 import { Link } from "react-router-dom";
 import Navbar from "../../Components/NavBar/navbar.jsx"; 
 import Footer from "../../Components/Footer/footer.jsx"; 
-
+import { VscSearch } from "react-icons/vsc";
 // Importando as imagens
 import Estacao from '../../Assets/Belem/Estação.png';
 import Teatro from '../../Assets/Belem/Teatro.png';
 import Veropeso from '../../Assets/Belem/Ver-o-peso.png';
 
 const Inicio = () => {
-  const images = [Estacao, Teatro, Veropeso];
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [images.length]);
-
   const comentarios = [
     { nome: "Ana Silva", texto: "Adorei a experiência, super intuitivo e fácil de usar!" },
     { nome: "João Pereira", texto: "Consegui explorar o site rapidamente, recomendo a todos!" },
     { nome: "Mariana Costa", texto: "Visual muito agradável e funcional, ótima navegação." }
   ];
+  const [searchTerm, setSearchTerm] = useState('');
+  
+    const handleSearch = (e) => {
+      e.preventDefault();
+      console.log("Buscando:", searchTerm); // aqui você pode integrar com API ou filtro
+    };
 
   return (
     <div>
       <Navbar />
 
       <main>
-        <div className="banner">
-          <div className="banner-text">
-            <h1>Frase de Efeito Aqui</h1>
-          </div>
-          <div className="banner-carousel">
-            <div className="carousel">
-              <img src={images[currentIndex]} alt={`Imagem ${currentIndex + 1}`} />
-            </div>
-          </div>
-        </div>
+       <div className="banner">
+  <div className="banner-overlay">
+
+    <h1 className="banner-title">Sua frase de efeito aqui</h1>
+    
+    <form className="banner-search" onSubmit={handleSearch}>
+             <input
+               type="text"
+               placeholder="Buscar projetos, habilidades ou palavras-chave..."
+               value={searchTerm}
+               onChange={(e) => setSearchTerm(e.target.value)}
+             />
+             <button type="submit"><VscSearch size={24} /></button>
+           </form>
+  </div>
+</div>
+
 
         <div className="escolha">
           <div className="card-escolha">
