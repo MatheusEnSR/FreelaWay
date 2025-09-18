eeb178d9bd7e2784bbe5d8b554b51b15fb54b371
Rodrigo Bessa <167825814+RatoFaice@users.noreply.github.com>
2025-09-18 18:44:31 -0300

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Employer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+    # Adicione outros campos relevantes aqui
+    
+    def __str__(self):
+        return self.name
